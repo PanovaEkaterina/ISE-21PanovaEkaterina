1815fabc801cb7bcd725ef425d46b958d1df4873
PanovaEkaterina <31787564+PanovaEkaterina@users.noreply.github.com>
2017-11-01 10:37:46 +0300

    Add files via upload

---
+++ WindowsFormsApplicationLab1/WindowsFormsApplicationLaba1/Stove.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplicationLaba1
+{
+    class Stove
+    {
+        private Pan pan;
+
+        private bool state = false;
+        public bool State { set { state = value; } get { return state; } }
+
+        public Pan Pan { set { pan = value; } get { return pan; } }
+
+        public void Cook()
+        {
+            if (State)
+            {
+                while (!pan.IsReadyPan())
+                {
+                    pan.Cook();
+                }
+            }
+        }
+    }
+}
