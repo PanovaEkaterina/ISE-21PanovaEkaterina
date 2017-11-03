c936bb04984c575804d5612cb47591566158782d
PanovaEkaterina <31787564+PanovaEkaterina@users.noreply.github.com>
2017-11-03 17:27:43 +0300

    Add files via upload

---
+++ TPlaba2_Java/src/Diamond.java
@@ -0,0 +1,30 @@
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class Diamond extends Adamant {
+	private boolean facet;
+	private Color dopColor;
+
+	public Diamond(int weight, int price, int hardness, Color color, boolean facet, Color dopColor) {
+		super(weight, price, hardness, color);
+		this.facet = facet;
+		this.dopColor = dopColor;
+	}
+
+	protected void drawAdamant(Graphics g) {
+		super.drawAdamant(g);
+
+		if (facet) {
+			g.setColor(dopColor);
+			g.drawLine(srartRosX - 30, srartRosY - 30, srartRosX + 30, srartRosY - 30);
+			g.drawLine(srartRosX - 25, srartRosY - 35, srartRosX - 13, srartRosY - 30);
+			g.drawLine(srartRosX, srartRosY - 35, srartRosX - 13, srartRosY - 30);
+			g.drawLine(srartRosX, srartRosY - 35, srartRosX + 13, srartRosY - 30);
+			g.drawLine(srartRosX + 25, srartRosY - 35, srartRosX + 13, srartRosY - 30);
+			g.drawLine(srartRosX - 10, srartRosY - 30, srartRosX, srartRosY);
+			g.drawLine(srartRosX + 13, srartRosY - 30, srartRosX, srartRosY);
+		}
+
+	}
+
+}
