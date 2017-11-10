497ed66534e3cdf8a95b4a97a2661399d8b70ab6
PanovaEkaterina <31787564+PanovaEkaterina@users.noreply.github.com>
2017-11-11 01:25:02 +0300

    Add files via upload

---
+++ TPlaba1_Java/src/Dough.java
@@ -0,0 +1,58 @@
+public class Dough {
+	private Egg[] eggs;
+	private Sugar sugar;
+	private Flour flour;
+
+	public void Init(int countegg) {
+		eggs = new Egg[countegg];
+	}
+
+	public void AddEgg(Egg e) {
+		for (int i = 0; i < eggs.length; i++) {
+			if (eggs[i] == null) {
+				eggs[i] = e;
+				return;
+			}
+		}
+	}
+
+	public void AddSugar(Sugar s) {
+		sugar = s;
+		// sugar.GetCountSugar() = true;
+	}
+
+	public void AddFlour(Flour f) {
+		flour = f;
+		// flour.Count_Flour = true;
+	}
+
+	public boolean Check() {
+		if (eggs == null) {
+			return false;
+		}
+		if (eggs.length == 0) {
+			return false;
+		}
+		for (int i = 0; i < eggs.length; ++i) {
+			if (eggs[i].GetHave_shell_egg()) {
+				return false;
+			}
+		}
+
+		if (eggs.length < 1)
+			return false;
+
+		for (int i = 0; i < eggs.length; ++i) {
+			if (eggs[i] == null)
+				return false;
+		}
+
+		if (sugar == null)
+			return false;
+
+		if (flour == null)
+			return false;
+
+		return true;
+	}
+}
